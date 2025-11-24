f79e7193b1fb20d1f6b0bc699f83a99731645623
AnonBergen <skatthebest@gmail.com>
2025-11-24 20:55:12 +0400

    first commit mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+}
+
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+
+for (let i = 1; i <= 9; i++) {
+    const hole = getHole(i);
+
+    hole.onclick = function () {
+   
+        if (hole.className.includes('hole_has-mole')) {
+            dead.textContent = Number(dead.textContent) + 1;
+        } else {
+            lost.textContent = Number(lost.textContent) + 1;
+        }
+
+        if (Number(dead.textContent) === 10) {
+            alert("Вы победили!");
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
+
+        if (Number(lost.textContent) === 5) {
+            alert("Вы проиграли!");
+            dead.textContent = 0;
+            lost.textContent = 0;
+        }
+    };
+}
